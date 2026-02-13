debce103690fff06b3910c9e6b8061982395286a
sisco-exp <sisco-exp@users.noreply.github.com>
2026-02-13 16:55:22 +0000

    chore(protobuf-go) : Auto-generated by Github Action

--- protogen/go/bank/service.pb.go
+++ protogen/go/bank/service.pb.go
@@ -24,24 +24,29 @@ var File_proto_bank_service_proto protoreflect.FileDescriptor
 
 const file_proto_bank_service_proto_rawDesc = "" +
 	"\n" +
-	"\x18proto/bank/service.proto\x12\x04bank\x1a\x1dproto/bank/type/account.proto\x1a\x1eproto/bank/type/exchange.proto2\xac\x01\n" +
+	"\x18proto/bank/service.proto\x12\x04bank\x1a\x1dproto/bank/type/account.proto\x1a\x1eproto/bank/type/exchange.proto\x1a!proto/bank/type/transaction.proto2\xf4\x01\n" +
 	"\vBankService\x12N\n" +
 	"\x11GetCurrentBalance\x12\x1b.bank.CurrentBalanceRequest\x1a\x1c.bank.CurrentBalanceResponse\x12M\n" +
-	"\x12FetchExchangeRates\x12\x19.bank.ExchangeRateRequest\x1a\x1a.bank.ExchangeRateResponse0\x01B5Z3github.com/sisco-exp/my-grpc-proto/protogen/go/bankb\x06proto3"
+	"\x12FetchExchangeRates\x12\x19.bank.ExchangeRateRequest\x1a\x1a.bank.ExchangeRateResponse0\x01\x12F\n" +
+	"\x15SummarizeTransactions\x12\x11.bank.Transaction\x1a\x18.bank.TransactionSummary(\x01B5Z3github.com/sisco-exp/my-grpc-proto/protogen/go/bankb\x06proto3"
 
 var file_proto_bank_service_proto_goTypes = []any{
 	(*CurrentBalanceRequest)(nil),  // 0: bank.CurrentBalanceRequest
 	(*ExchangeRateRequest)(nil),    // 1: bank.ExchangeRateRequest
-	(*CurrentBalanceResponse)(nil), // 2: bank.CurrentBalanceResponse
-	(*ExchangeRateResponse)(nil),   // 3: bank.ExchangeRateResponse
+	(*Transaction)(nil),            // 2: bank.Transaction
+	(*CurrentBalanceResponse)(nil), // 3: bank.CurrentBalanceResponse
+	(*ExchangeRateResponse)(nil),   // 4: bank.ExchangeRateResponse
+	(*TransactionSummary)(nil),     // 5: bank.TransactionSummary
 }
 var file_proto_bank_service_proto_depIdxs = []int32{
 	0, // 0: bank.BankService.GetCurrentBalance:input_type -> bank.CurrentBalanceRequest
 	1, // 1: bank.BankService.FetchExchangeRates:input_type -> bank.ExchangeRateRequest
-	2, // 2: bank.BankService.GetCurrentBalance:output_type -> bank.CurrentBalanceResponse
-	3, // 3: bank.BankService.FetchExchangeRates:output_type -> bank.ExchangeRateResponse
-	2, // [2:4] is the sub-list for method output_type
-	0, // [0:2] is the sub-list for method input_type
+	2, // 2: bank.BankService.SummarizeTransactions:input_type -> bank.Transaction
+	3, // 3: bank.BankService.GetCurrentBalance:output_type -> bank.CurrentBalanceResponse
+	4, // 4: bank.BankService.FetchExchangeRates:output_type -> bank.ExchangeRateResponse
+	5, // 5: bank.BankService.SummarizeTransactions:output_type -> bank.TransactionSummary
+	3, // [3:6] is the sub-list for method output_type
+	0, // [0:3] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
 	0, // [0:0] is the sub-list for extension extendee
 	0, // [0:0] is the sub-list for field type_name
@@ -54,6 +59,7 @@ func file_proto_bank_service_proto_init() {
 	}
 	file_proto_bank_type_account_proto_init()
 	file_proto_bank_type_exchange_proto_init()
+	file_proto_bank_type_transaction_proto_init()
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
